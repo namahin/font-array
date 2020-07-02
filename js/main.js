01db9a9d23ed835d00ee549a3112e39a2d89121d
namahin <mahin0w1@gmail.com>
2020-07-03 00:03:48 +0600

    font preview added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 // alphabat category js
-$(function(){
-	$('#fontFilter').listnav({initHidden: false});
-	$('.filter a').click(function(e) {
+$(function () {
+	$('#fontFilter').listnav({
+		initHidden: false,
+		noMatchText: 'No font for this Alphabet'
+	});
+	$('.filter a').click(function (e) {
 		e.preventDefault();
+
 	});
 });
+
+
+// add .italic-active class & remove .italic class
+$('.italic').click(function () {
+	var $this = $(this);
+	if ($this.hasClass('italic')) {
+		$this.removeClass('italic').addClass('italic-active');
+	} else if ($this.hasClass('italic-active')) {
+		$this.removeClass('italic-active').addClass('italic');
+	} else {
+		$this.addClass('italic')
+	}
+})
